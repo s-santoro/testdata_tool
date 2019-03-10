f24bf2f38023984a55d677119e3bb971bcd3c119
s-santoro <36934087+s-santoro@users.noreply.github.com>
2019-03-11 00:17:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const fs = require("fs");
+// log all files in ./files/
+let files = [];
+let fileCounter = 0;
+if(files.length === 0)
+{
+    fs.readdir(`${__dirname}/files/`, {}, (err, filesTemp) => {
+        if (err) throw err;
+        files = filesTemp;
+        fs.readFile(`${__dirname}/files/${files[fileCounter]}`, "utf-8", (err, data) => {
+            if (err) throw err;
+            let json = JSON.parse(data);
+            let html = json.content;
+            let doc = document.getElementById('frame').contentWindow.document;
+            doc.open();
+            doc.write(html);
+            doc.close();
+            document.getElementById('title').innerText = json.url;
+        });
+        fs.mkdir(`${__dirname}/files/pos`, (err) => {
+            if (err) throw err;
+        });
+        fs.mkdir(`${__dirname}/files/neg`, (err) => {
+            if (err) throw err;
+        });
+    });
+}
+
+const yes = document.getElementById('yes');
+const no = document.getElementById('no');
+
+
+yes.addEventListener('click', event => {
+    // copy file to pos-folder
+    fs.copyFile(
+        `${__dirname}/files/${files[fileCounter]}`,
+        `${__dirname}/files/pos/${files[fileCounter]}`,
+        (err) => {
+            if(err) throw err;
+            console.log("copied to pos");
+    });
+    if(fileCounter < files.length) {
+        fileCounter++;
+    }else{
+        alert("all files checked");
+    }
+    // load next file
+    fs.readFile(`${__dirname}/files/${files[fileCounter]}`, "utf-8", (err, data) => {
+        if (err) throw err;
+        let json = JSON.parse(data);
+        let html = json.content;
+        let doc = document.getElementById('frame').contentWindow.document;
+        doc.open();
+        doc.write(html);
+        doc.close();
+        document.getElementById('title').innerText = json.url;
+    });
+});
+no.addEventListener('click', event => {
+    console.log(files);
+    // copy file to neg-folder
+    fs.copyFile(
+        `${__dirname}/files/${files[fileCounter]}`,
+        `${__dirname}/files/neg/${files[fileCounter]}`,
+        (err) => {
+            if(err) throw err;
+            console.log("copied to neg");
+    });
+    if(fileCounter < files.length) {
+        fileCounter++;
+    }else{
+        alert("all files checked");
+    }
+    // load next file
+    fs.readFile(`${__dirname}/files/${files[fileCounter]}`, "utf-8", (err, data) => {
+        if (err) throw err;
+        let json = JSON.parse(data);
+        let html = json.content;
+        let doc = document.getElementById('frame').contentWindow.document;
+        doc.open();
+        doc.write(html);
+        doc.close();
+        document.getElementById('title').innerText = json.url;
+    });
+});
